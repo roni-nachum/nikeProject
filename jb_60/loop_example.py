bbabaa0c4d6f2ad637b86db0cd49403300039949
roni-nachum <roni.nachum1311@gmail.com>
2025-09-06 19:55:32 +0300

    delete unneeded files

--- jb_60/loop_example.py
+++
@@ -1,8 +0,0 @@
-
-temp = 0
-for i in range(0,4):
-    print(i)
-    temp = temp +5
-    print(f'the new value of temp is {temp}')
-
-print('test end')
--- jb_60/my_first_test.py
+++
@@ -1,9 +0,0 @@
-
-first_name = 'Roni'
-last_name = 'Nachum'
-age = 21
-
-print ('my name is Roni')
-# for writing notes in the code we will use the #, ctrl plus ? will make every line that we mark note and
-# and when we want to turn the line back from note we will use ctrl plus ?.
-print(f'hi {first_name} {last_name} is {age} years old')
